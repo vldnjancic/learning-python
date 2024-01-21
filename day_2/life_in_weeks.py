99582203e95e9d0b06dea33f625fd1572188b741
vldnjancic <vldnjancic@users.noreply.github.com>
2024-01-21 17:29:02 +0100

    A life in weeks calculator

---
+++ day_2/life_in_weeks.py
@@ -0,0 +1,8 @@
+age = int(input("What is your current age? "))
+
+years_left = 90 - age
+days = years_left * 365
+weeks = years_left * 52
+months = years_left * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
