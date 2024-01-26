fbab4efd30c1fa33890f43ec932ebc038809a2d2
vldnjancic <vldnjancic@users.noreply.github.com>
2024-01-26 22:49:39 +0100

    I know there is a shorter solution, but..

---
+++ day_3/love_calculator.py
@@ -0,0 +1,24 @@
+print("Welcome to the love calculator!")
+name1 = input("What is your name? \n").lower()
+name2 = input("What is their name? \n").lower()
+
+t = name1.count("t") + name2.count("t")
+r = name1.count("r") + name2.count("r")
+u = name1.count("u") + name2.count("u")
+e = name1.count("e") + name2.count("e")
+l = name1.count("l") + name2.count("l")
+o = name1.count("o") + name2.count("o")
+v = name1.count("v") + name2.count("v")
+
+true = str(t + r + u + e)
+love = str(l + o + v + e)
+score = int(true + love)
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and menthos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
+
+    
