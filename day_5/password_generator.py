5852b9691efdd99ab848563ddbb91abd62d55b62
vldnjancic <vldnjancic@users.noreply.github.com>
2024-02-06 09:38:19 +0100

    Finished the Password Generator project using the For loops

--- day_5/password_generator.py
+++ day_5/password_generator.py
@@ -9,8 +9,14 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-passw = ""
-for letter in range(0, nr_letters):
-    passw += letters[nr_letters]
+password = []
+for letter in range(nr_letters):
+    password += random.choice(letters)
+for number in range(nr_numbers):
+    password += random.choice(numbers)
+for symbol in range(nr_symbols):
+    password += random.choice(symbols)
 
-print(passw)
+random.shuffle(password)
+new_pass = ''.join(password)
+print(new_pass)
