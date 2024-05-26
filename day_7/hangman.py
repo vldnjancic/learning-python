9c1d4dbca40c7d7ab7b4c4e689f33df2bfa2b017
vldnjancic <885098+vldnjancic@users.noreply.github.com>
2024-05-27 00:23:55 +0200

    My version of challenge 3

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,3 +1,4 @@
+from os import replace
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
@@ -8,22 +9,30 @@ chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ").lower()
+# guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 print(chosen_word)
 
-hidden_word = ""
+hidden_word = []
 for i in chosen_word:
     hidden_word += "_"
 print(hidden_word)
 
-for letter in chosen_word:
-    if letter == guess:
-        print("correct")
-    else:
-        print("Wrong answer!")
+
+def position():
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            hidden_word[position] = guess
+
+while "_" in hidden_word:
+    guess = input("Guess a letter: ").lower()
+    position()
+    print(hidden_word)
+    if "_" not in hidden_word:
+        print("You Win!")
+        exit()
 
 # if guess in chosen_word:
 #     print("Correct")
