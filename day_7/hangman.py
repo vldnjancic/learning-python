de84eff5758e7037d88a57e1ceef77f0cabdfef9
vldnjancic <885098+vldnjancic@users.noreply.github.com>
2024-05-27 14:41:01 +0200

    Final version of hangman

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,38 +1,52 @@
-from os import replace
 import random
+from hangman_art import stages, clear_screen, logo
+from hangman_words import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 chosen_word = random.choice(word_list)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 # guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-print(chosen_word)
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 hidden_word = []
 for i in chosen_word:
     hidden_word += "_"
-print(hidden_word)
 
+print(logo)
+end_of_game = False
+lives = 6
 
-def position():
+while end_of_game != True:
+    guess = input("Guess a letter: ").lower()
+
+    clear_screen()
+
+    if guess in hidden_word:
+        print(f"You've already entered {guess}")
+
     for position in range(len(chosen_word)):
         if chosen_word[position] == guess:
             hidden_word[position] = guess
 
-while "_" in hidden_word:
-    guess = input("Guess a letter: ").lower()
-    position()
-    print(hidden_word)
+    if guess not in chosen_word:
+        print(f"You've guessed {guess}, it's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print(f"You lose. The answer was {chosen_word}")
+
+    print(f"{''.join(hidden_word)}")
+
     if "_" not in hidden_word:
+        end_of_game = True
         print("You Win!")
-        exit()
+
+    print(stages[lives])
+
 
 # if guess in chosen_word:
 #     print("Correct")
