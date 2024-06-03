12e27f0819ebe065da0aa0c38863b7cc0975ecb3
vldnjancic <885098+vldnjancic@users.noreply.github.com>
2024-06-03 20:41:45 +0200

    Duplicated the alphabet

--- day_8/ceasar_cipher.py
+++ day_8/ceasar_cipher.py
@@ -1,8 +1,62 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
 
-choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n" )
+choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 message = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 
 def encrypt(message, shift):
     cipher_text = ""
@@ -12,5 +66,6 @@ def encrypt(message, shift):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(f"The encoded text is: {cipher_text}")
+
 
 encrypt(message=message, shift=shift)
