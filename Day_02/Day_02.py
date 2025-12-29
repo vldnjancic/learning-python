3c40f548daa2babb76c2d00e08406eafa27103f6
vldnjancic <vldnjancic@users.noreply.github.com>
2025-12-29 20:56:35 +0100

    Finished the Day 2 Project - Tip Calculator

--- Day_02/Day_02.py
+++ Day_02/Day_02.py
@@ -1,3 +1,7 @@
-number = input("Type a two digit number:\n")
-result = int(number[0]) + int(number[1])
-print(f"The score is: {result}")
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? "))
+percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+nr_people = int(input("How many people are splitting the bill? "))
+split_bill = total_bill / nr_people
+bill = round((split_bill) * (percentage_tip * 0.01) + split_bill, 2)
+print(f"Each person should pay: ${bill}")
---
+++ Day_02/life_in_weeks.py
@@ -0,0 +1,6 @@
+age = int(input("What is your current age? "))
+years = 90 - age
+days = years * 365
+weeks = years * 52
+months = years * 12
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
